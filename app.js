d82cfd3a98ef434639748882e150a82c7907823d
arielwierz <arielwierzbinski@gmail.com>
2022-06-02 10:03:54 -0300

    ok

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const hostname = "127.0.0.1";
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World\n");
+});
+//Código nuevo
+const MongoClient = require("mongodb").MongoClient;
+const uri = "mongodb+srv://arielwierzbinski:cKcETLzwebVZFtxL@db-tp-dai.ehzn4.mongodb.net/?retryWrites=true&w=majority";
+MongoClient.connect(uri, {useUnifiedTopology: true }, (err, client) => {
+  if (err) console.log("Error occurred connecting to MongoDB...");
+  console.log("Connected to MongoDB!");
+});
+//Fin código nuevo
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
